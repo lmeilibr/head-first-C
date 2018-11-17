bff0a32d2d15761f75e823dcf474fa3101beaa0d
lmeilibr <leandro.meili@gmail.com>
2018-11-17 12:11:47 -0200

    southeast code

---
+++ ch02/southeast.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void go_south_east(int *, int *);
+int main(){
+	int latitude = 32;
+	int longitude = -64;
+
+	go_south_east(&latitude, &longitude);
+	printf("Avast! Now at: [%d, %d]\n", latitude, longitude);
+	return 0;
+}
+void go_south_east(int *lat, int *lon){
+	*lat = *lat -1;
+	*lon = *lon +1;
+}
